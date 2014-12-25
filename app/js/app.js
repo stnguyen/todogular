2f62df9067d45f0d3bb730932d2fb85a38026a6f
Stefan Nguyen <stefan@vinova.sg>
2014-12-25 11:22:29 +0700

    implement TodoCtrl add(), updateStatus() and clearCompletedItems()

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,21 @@
-'use strict';
+var app = angular.module("app", []);
 
-/* App Module */
+app.controller("TodoCtrl", [function() {
+	var vm = this;
+
+	vm.todos = [];
+
+	vm.add = function(item) {
+		vm.todos.push(item);
+	};
+
+	vm.updateStatus = function(id, done) {
+		vm.todos[id].done = done;
+	}
+
+	vm.clearDoneItems = function() {
+		vm.todos = vm.todos.filter(function(item) {
+			return !item.done
+		});
+	}
+}]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,11 +1,57 @@
-'use strict';
+describe("TodoCtrl", function() {
+	var scope;
 
-/* jasmine specs for controllers go here */
+	beforeEach(function() {
+		this.addMatchers({
+			toEqualData: function(expected) {
+				return angular.equals(this.actual, expected);
+			}
+		});
+	});
 
-describe('controllers', function() {
+	beforeEach(module("app"));
 
-  it("should do something", function() {
+	beforeEach(inject(function($controller, $rootScope) {
+		scope = $rootScope.$new();
+		$controller("TodoCtrl as vm", { $scope: scope });
+	}));
 
-  });
+	it("should create todos model with zero item", function() {
+		expect(scope.vm.todos.length).toBe(0);
+	});
 
+	describe("add(item)", function() {
+		it("should add the item to todo list", function() {
+			scope.vm.todos = [];
+
+			var item = { title: "new item", done: false };
+			scope.vm.add(item);
+			expect(scope.vm.todos).toEqualData([item]);
+		});
+	});
+
+	describe("updateStatus(id, done)", function() {
+		it("should mark the item with given id as done", function() {
+			scope.vm.todos = [{ title: "a", done: false}, { title: "b", done: false}];
+
+			scope.vm.updateStatus(1, true);
+
+			expect(scope.vm.todos[0].done).toBe(false);
+			expect(scope.vm.todos[1].done).toBe(true);
+
+			scope.vm.updateStatus(1, false);
+			expect(scope.vm.todos[0].done).toBe(false);
+			expect(scope.vm.todos[1].done).toBe(false);
+		});
+	});
+
+	describe("clearDoneItems()", function() {
+		it("should delete items marked as done", function() {
+			scope.vm.todos = [{ title: "a", done: true}, { title: "b", done: false}, { title: "c", done: true}];
+
+			scope.vm.clearDoneItems();
+
+			expect(scope.vm.todos).toEqualData([{ title: "b", done: false}]);
+		});
+	});
 });
